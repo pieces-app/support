10b837d700f173120a5f4d0c14f70b0e730cc1b6
Mason Williams <mason@pieces.app>
2024-04-16 12:54:06 -0400

    chore: Normalize Early Access Program label casing

--- .github/labeler/index.js
+++ .github/labeler/index.js
@@ -32,7 +32,7 @@ async function run() {
 
   // Check if Early Access Program checkbox is checked
   if (issueBody.includes('- [x] Yes, this is related to an Early Access Program feature.')) {
-    labels.push('Early Access Program');
+    labels.push('early access program');
   }
 
   if (labels.length) await octokit.issues.addLabels({
